16b5211370467f853776cd18f38630a70bd798b1
minawadee <WADe3MINa981994>
2021-04-15 19:35:51 +0200

    - Marvel Character List Adapter  - Marvel Character List Ui  - Marvel List Connection with the related fragment

--- app/src/main/java/com/extremesolution/marvelapp/ui/adapters/CharactersAdapter.kt
+++ app/src/main/java/com/extremesolution/marvelapp/ui/adapters/CharactersAdapter.kt
@@ -1,11 +1,23 @@
 package com.extremesolution.marvelapp.ui.adapters
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.extremesolution.marvelapp.R
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.character_list_for_adapter_layout.view.*
+
 
 class CharactersAdapter(var context: Context) :
     RecyclerView.Adapter<CharactersAdapter.ViewHolder>() {
@@ -20,6 +32,11 @@ class CharactersAdapter(var context: Context) :
 
        // holder.itemView.MarvelCharacterNameTV.text = "add"
 
+        //To make bg image blur
+        Glide.with(context).load(R.drawable.mcu_background)
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
+            .into(holder.itemView.MarvelTextBG)
+
     }
 
     override fun getItemCount(): Int {
@@ -27,5 +44,4 @@ class CharactersAdapter(var context: Context) :
     }
 
     class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!)
-
 }
--- app/src/main/java/com/extremesolution/marvelapp/ui/home/MarvelCharactersFragment.kt
+++ app/src/main/java/com/extremesolution/marvelapp/ui/home/MarvelCharactersFragment.kt
@@ -1,32 +1,81 @@
 package com.extremesolution.marvelapp.ui.home
 
 import android.annotation.SuppressLint
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.DrawableCompat
 import com.bumptech.glide.Glide
 import com.extremesolution.marvelapp.R
 import com.extremesolution.marvelapp.data.Repositories.MarvelCharactersRepository
 import com.extremesolution.marvelapp.data.network.ApiInterface
 import com.extremesolution.marvelapp.databinding.MarvelCharactersLayoutBinding
+import com.extremesolution.marvelapp.ui.adapters.CharactersAdapter
 import com.extremesolution.marvelapp.ui.base.BaseFragment
 import com.extremesolution.marvelapp.ui.home.ViewModels.MarvelCharactersViewModel
+import com.jcodecraeer.xrecyclerview.XRecyclerView.LoadingListener
+import kotlinx.android.synthetic.main.marvel_characters_layout.*
 
 
 class MarvelCharactersFragment :
     BaseFragment<MarvelCharactersViewModel, MarvelCharactersLayoutBinding, MarvelCharactersRepository>() {
+
+    var currentPage: Int = 1
+    var lastPage: Int = -1
 
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         Glide.with(requireContext()).load(R.drawable.search_icon).into(binding.searchButton)
+
+
+        //To set loading style of load more and refresh
+        binding.MarvelCharactersListRVID.setLoadingMoreProgressStyle(3)
+        binding.MarvelCharactersListRVID.setRefreshProgressStyle(3)
+        //Recyclerview load more code
+        binding.MarvelCharactersListRVID.setLoadingListener(object : LoadingListener {
+            override fun onRefresh() {
+                currentPage = 1
+                lastPage = -1
+                updateMarvelList()
+                binding.MarvelCharactersListRVID.setNoMore(false)
+                binding.MarvelCharactersListRVID.setLoadingMoreEnabled(true)
+            }
+
+            override fun onLoadMore() {
+                if (currentPage <= lastPage) {
+                    updateMarvelList()
+                } else {
+                    binding.MarvelCharactersListRVID.setNoMore(false)
+                }
+            }
+        })
+
+        //Initialize the RV
+        updateMarvelList()
     }
 
+    private fun updateMarvelList(/*list: ArrayList<ListUpdatesDataArrayModel>*/) {
+        var adapter: CharactersAdapter? = null
+        if (currentPage == 1) {
+
+            MarvelCharactersListRVID.apply {
+                adapter = CharactersAdapter(activity?.applicationContext!!/*, list*/)
+                MarvelCharactersListRVID.adapter = adapter
+            }
+
+        } else {
+
+        }
+
+        adapter?.notifyDataSetChanged()
+        MarvelCharactersListRVID.loadMoreComplete()
+        MarvelCharactersListRVID.refreshComplete()
+
+        if (currentPage <= lastPage) {
+            currentPage += 1
+        }
+    }
 
     override fun getViewModel(): Class<MarvelCharactersViewModel> =
         MarvelCharactersViewModel::class.java
